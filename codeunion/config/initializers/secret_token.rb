9471112c0e476a52aa212ef209bc5ac7f6314685
elliotec <elliotecweb@gmail.com>
2014-02-26 11:18:38 -0800

    initial commit

---
+++ codeunion/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Codeunion::Application.config.secret_key_base = '0e6012295935b14ab298c1a0fe25b701a2a7738b72f0adea59b970fda8bad9b510b93ab3b3adc6dca8a7d4ded659504a173c81545b4cc369fe7cdcf54070892f'
---
+++ codeunion/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Codeunion::Application.config.session_store :cookie_store, key: '_codeunion_session'
