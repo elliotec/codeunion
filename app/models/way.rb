2c231f016562da0f6eff387f01d06950ee17906e
Joel Smith <joely5525@gmail.com>
2014-03-03 15:44:39 -0800

    fixed Way strong params and validated input of category

--- app/models/way.rb
+++ app/models/way.rb
@@ -1,3 +1,4 @@
 class Way < ActiveRecord::Base
   belongs_to :language
+  validates_inclusion_of :category, :in => ['book','video','mooc', 'tutorial', 'bootcamp', 'blog post', 'game', 'university']
 end
