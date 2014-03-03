32127f4ce356e3b6f295179ac9c795a1741be271
kaxla <kaylas.email.address@gmail.com>
2014-03-03 15:21:43 -0800

    john fixed CSS

--- test/features/auth/sign_up_test.rb
+++ test/features/auth/sign_up_test.rb
@@ -7,8 +7,7 @@ feature "creating a user account" do
     click_on "Sign up"
     fill_in "Email",  with: "email@email.com"
     fill_in "Password", with: "password"
-    fill_in "Password confirmation", with: "password"
-    click_on "Sign up"
-    page.text.must_include "Welcome! You have signed up successfully"
+    click_button "Sign up"
+    page.text.must_include "Welcome! You have signed up successfully."
   end
 end
