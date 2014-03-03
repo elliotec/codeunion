5d6122cd23bcded46bcf2fafcbaa6d1b83a7adeb
kaxla <kaylas.email.address@gmail.com>
2014-03-03 15:44:35 -0800

    add sign_in test

--- test/features/auth/sign_in_test.rb
+++ test/features/auth/sign_in_test.rb
@@ -1,3 +1,13 @@
 require "test_helper"
 
+feature "signing in to an existing account" do
+  scenario "exisiting user signing in" do
+    visit "/"
+    click_on "Sign in"
+    fill_in "Email", with: users(:one).email
+    fill_in "Password", with: "password"
+    click_button "Sign in"
+    page.text.must_include "Signed in successfully"
 
+  end
+end
--- test/features/auth/sign_up_test.rb
+++ test/features/auth/sign_up_test.rb
@@ -3,7 +3,6 @@ require "test_helper"
 feature "creating a user account" do
   scenario "user creating an account" do
     visit "/"
-    save_and_open_page
     click_on "Sign up"
     fill_in "Email",  with: "email@email.com"
     fill_in "Password", with: "password"
