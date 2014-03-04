f79346d07f42e9f48aa9c7f6f69e09c65f5c3bab
John Jensen MBP <johncjensen@hotmail.com>
2014-03-04 10:02:23 -0800

    move delete button into show and rewrite test accordingly

--- test/features/languages/delete_language_test.rb
+++ test/features/languages/delete_language_test.rb
@@ -2,10 +2,10 @@ require "test_helper"
 
 feature "deleting a language" do
   scenario "a language can be deleted from index page" do
-    visit languages_path
+    visit language_path(languages(:ruby))
     first(:link, "Delete").click
     save_and_open_page
-    page.wont_have_content languages(:ruby).name
+    page.wont_have_content "Ruby"
     page.text.must_include "Languages"
   end
 end
