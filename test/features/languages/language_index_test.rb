f046ff1391309205cc0ff346a043ef70d0ee4941
kaxla <kaylas.email.address@gmail.com>
2014-03-04 09:48:51 -0800

    all language tests except edit/delete

--- test/features/languages/language_index_test.rb
+++ test/features/languages/language_index_test.rb
@@ -1,2 +1,9 @@
 require "test_helper"
 
+feature "language index" do
+  scenario "visiting the language index page" do
+    visit languages_path
+    page.text.must_include languages(:ruby).name
+    page.text.must_include languages(:ruby).description
+  end
+end
--- test/features/languages/show_language_test.rb
+++ test/features/languages/show_language_test.rb
@@ -1,1 +1,9 @@
 require "test_helper"
+
+feature "showing a single language" do
+  scenario "user visits single language page" do
+    visit language_path(languages(:ruby))
+    page.text.must_include languages(:ruby).name
+    page.text.must_include languages(:ruby).description
+  end
+end
