5046643c39e278ebda56d0eab794f9195aa1dfa5
elliotec <elliotecweb@gmail.com>
2014-03-04 12:00:09 -0800

    work on tests

--- test/features/languages/creating_a_language_test.rb
+++ test/features/languages/creating_a_language_test.rb
@@ -3,11 +3,11 @@ require "test_helper"
 feature "Creating a Resource" do
   scenario "a resource can be created" do
     visit new_resource_path
-    fill_in "Name", with: resource(:ruby).name
-    fill_in "Description", with: resource(:ruby).description
+    fill_in "Name", with: resources(:codefellows).name
+    fill_in "Body", with: resources(:codefellows).body
     click_on "Create Resource"
     page.text.must_include "Resource was successfully created"
-    page.text.must_include resource(:ruby).name
-    page.text.must_include resource(:ruby).description
+    page.text.must_include resources(:codefellows).name
+    page.text.must_include resources(:codefellows).body
   end
 end
--- test/features/languages/delete_language_test.rb
+++ test/features/languages/delete_language_test.rb
@@ -1,11 +1,11 @@
 require "test_helper"
 
-feature "deleting a language" do
-  scenario "a language can be deleted from index page" do
-    visit language_path(languages(:ruby))
-    first(:link, "Delete").click
+feature "deleting a resource" do
+  scenario "a resource can be deleted from index page" do
+    visit resource_path(resources(:poodr))
     save_and_open_page
-    page.wont_have_content "Ruby"
-    page.text.must_include "Languages"
+    click_on "Delete"
+    page.wont_have_content "book"
+    page.text.must_include "resources"
   end
 end
