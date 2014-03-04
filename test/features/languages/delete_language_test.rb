95d2943e1b8cb11582ddb42b7ed69c539df70f0c
kaxla <kaylas.email.address@gmail.com>
2014-03-03 21:39:32 -0800

    fill out languages tests, make ways test files

--- test/features/languages/delete_language_test.rb
+++ test/features/languages/delete_language_test.rb
@@ -1,5 +1,11 @@
 require "test_helper"
 
-visit languages_path
-name = languages(:ruby).name
-page.find("tbody tr:last")
+feature "deleting a language" do
+  scenario "a language can be deleted from index page" do
+    visit languages_path
+    within("div#service block")
+    click_on "Delete"
+    page.wont_have_content languages(:ruby).name
+    page.text.must_include "Languages"
+  end
+end
