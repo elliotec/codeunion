d28288463c9ef49fb77f9fcea08c3e16c5db64ac
elliotec <elliotecweb@gmail.com>
2014-03-04 09:52:23 -0800

    delete test fail

--- test/features/languages/delete_language_test.rb
+++ test/features/languages/delete_language_test.rb
@@ -3,8 +3,8 @@ require "test_helper"
 feature "deleting a language" do
   scenario "a language can be deleted from index page" do
     visit languages_path
-    within("div#service block")
-    click_on "Delete"
+    first(:link, "Delete").click
+    save_and_open_page
     page.wont_have_content languages(:ruby).name
     page.text.must_include "Languages"
   end
