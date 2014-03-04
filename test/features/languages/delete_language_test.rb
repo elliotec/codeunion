7b3d7104ea4e5c5d79ad7b3c1d80936e72cdc792
John Jensen MBP <johncjensen@hotmail.com>
2014-03-04 10:03:14 -0800

    remove launchy

--- test/features/languages/delete_language_test.rb
+++ test/features/languages/delete_language_test.rb
@@ -4,7 +4,6 @@ feature "deleting a language" do
   scenario "a language can be deleted from index page" do
     visit language_path(languages(:ruby))
     first(:link, "Delete").click
-    save_and_open_page
     page.wont_have_content "Ruby"
     page.text.must_include "Languages"
   end
