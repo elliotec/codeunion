49fb2fb3f3fcff78ac5cd1b84f0efd0e169ccd9b
Joel Smith <joely5525@gmail.com>
2014-03-04 15:07:06 -0800

    added redirect if top_voted route doesn't match

--- app/controllers/top_voted_controller.rb
+++ app/controllers/top_voted_controller.rb
@@ -2,6 +2,10 @@ class TopVotedController < ApplicationController
   def index
     @language = Language.where(name: params[:language].downcase).first
     @category = Category.where(name: params[:category].downcase).first
-    @resources = Resource.where(language_id: @language.id, category_id: @category.id)
+    if @langauge == nil && @category == nil
+      redirect_to root_path
+    else
+      @resources = Resource.where(language_id: @language.id, category_id: @category.id)
+    end
   end
 end
