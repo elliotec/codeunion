231ed6e9d507cd8e6f520a93e87d950c2827455c
elliotec <elliotecweb@gmail.com>
2014-03-04 12:20:50 -0800

    reroute resources resources routes

--- test/features/languages/delete_language_test.rb
+++ test/features/languages/delete_language_test.rb
@@ -3,7 +3,6 @@ require "test_helper"
 feature "deleting a resource" do
   scenario "a resource can be deleted from index page" do
     visit resource_path(resources(:poodr))
-    save_and_open_page
     click_on "Delete"
     page.wont_have_content "book"
     page.text.must_include "resources"
