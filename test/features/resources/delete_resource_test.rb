5ae6ef23b00259f48fea6d6ccd3c26d47abe0fe3
elliotec <elliotecweb@gmail.com>
2014-03-04 15:42:28 -0800

    pass all 8 tests

--- test/features/resources/delete_resource_test.rb
+++ test/features/resources/delete_resource_test.rb
@@ -3,9 +3,10 @@ require "test_helper"
 feature "deleting a resource" do
   scenario "a resource can be deleted from index page" do
     visit resource_path(resources(:poodr))
-    first(:link, "Delete").click
+    save_and_open_page
+    click_on "Destroy"
     save_and_open_page
     page.wont_have_content "book"
-    page.text.must_include "resources"
+    page.text.must_include "Resources"
   end
 end
--- test/features/resources/resource_index_test.rb
+++ test/features/resources/resource_index_test.rb
@@ -1,2 +1,9 @@
 require "test_helper"
 
+feature "visiting the resource index" do
+  scenario "with existing resources" do
+    visit resources_path
+
+    page.text.must_include "Resources"
+  end
+end
