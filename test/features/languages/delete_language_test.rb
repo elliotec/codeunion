c5365a7627dd278fbcb1096f7a2133b207b2b8cb
kaxla <kaylas.email.address@gmail.com>
2014-03-04 10:42:32 -0800

    All Language tests written and working

--- test/features/languages/delete_language_test.rb
+++ test/features/languages/delete_language_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 feature "deleting a language" do
-  scenario "a language can be deleted from index page" do
+  scenario "a language can be deleted from show page" do
     visit language_path(languages(:ruby))
     first(:link, "Delete").click
     page.wont_have_content "Ruby"
