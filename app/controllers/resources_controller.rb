95eaf77e1c4a70959b6e70035436cb0d793616ab
John Jensen MBP <johncjensen@hotmail.com>
2014-03-05 09:23:01 -0800

    remove scaffold comments

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,28 +1,21 @@
 class ResourcesController < ApplicationController
   before_action :set_resource, only: [:show, :edit, :update, :destroy]
   load_and_authorize_resource
-  # GET /resources
-  # GET /resources.json
+
   def index
     @resources = Resource.all
   end
 
-  # GET /resources/1
-  # GET /resources/1.json
   def show
   end
 
-  # GET /resources/new
   def new
     @resource = Resource.new
   end
 
-  # GET /resources/1/edit
   def edit
   end
 
-  # POST /resources
-  # POST /resources.json
   def create
     @resource = Resource.new(resource_params)
 
@@ -37,8 +30,6 @@ class ResourcesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /resources/1
-  # PATCH/PUT /resources/1.json
   def update
     respond_to do |format|
       if @resource.update(resource_params)
@@ -51,8 +42,6 @@ class ResourcesController < ApplicationController
     end
   end
 
-  # DELETE /resources/1
-  # DELETE /resources/1.json
   def destroy
     @resource.destroy
     respond_to do |format|
@@ -62,12 +51,10 @@ class ResourcesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_resource
       @resource = Resource.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def resource_params
       params.require(:resource).permit(:name, :body, :language_id, :category_id)
     end
