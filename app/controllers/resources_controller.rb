0f3cac34632418e517ef05ec3d824cddcda1ec8b
kaxla <kaylas.email.address@gmail.com>
2014-03-04 17:32:13 -0800

    add authorization to languages and categories

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,6 +1,6 @@
 class ResourcesController < ApplicationController
   before_action :set_resource, only: [:show, :edit, :update, :destroy]
-
+  load_and_authorize_resource
   # GET /resources
   # GET /resources.json
   def index
