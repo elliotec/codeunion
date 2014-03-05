550d708d878b002790f3fd8aa0943bb644f50951
elliotec <elliotecweb@gmail.com>
2014-03-04 17:12:13 -0800

    categories view

--- db/schema.rb
+++ db/schema.rb
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20140305001745) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "moderator"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
