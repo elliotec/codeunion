0396ae6d98a1f0e0f178a5bbd701ee024faafec3
Katie Brasfield <katiebrasfield@gmail.com>
2014-05-26 21:43:49 -0700

    add top nav

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,11 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require foundation
 //= require turbolinks
 //= require fixed-bootstrap.min
 //= require ckeditor/override
 //= require ckeditor/init
 //= require_tree .
+
+$(function(){ $(document).foundation(); });
