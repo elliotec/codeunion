29a947dac3afbdd23fd875d589a284e984708e16
Katie Brasfield <katiebrasfield@gmail.com>
2014-05-30 11:58:01 -0700

    edited seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,67 +6,62 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Language.create(name: "Ruby")
-Language.create(name: "Python")
-Language.create(name: "Javascript")
-Language.create(name: "PHP")
-Language.create(name: "Objective-C")
-Language.create(name: "Java")
-Language.create(name: "Other")
+ruby = Language.create(name: "Ruby")
+python = Language.create(name: "Python")
+javascript = Language.create(name: "Javascript")
+php = Language.create(name: "PHP")
+objectivec = Language.create(name: "Objective-C")
+java = Language.create(name: "Java")
+other = Language.create(name: "Other")
 
-Category.create(name: "Book")
-Category.create(name: "Bootcamp")
-Category.create(name: "Website")
-Category.create(name: "Blog")
-Category.create(name: "Game")
-Category.create(name: "Video")
-Category.create(name: "University")
+book = Category.create(name: "Book")
+bootcamp = Category.create(name: "Bootcamp")
+website = Category.create(name: "Website")
+blog = Category.create(name: "Blog")
+game = Category.create(name: "Game")
+video = Category.create(name: "Video")
+university = Category.create(name: "University")
 
 Resource.create(name: "Rails Guides",
                 body: "http://guides.rubyonrails.org/",
-                language_id: 1,
-                category_id: 3)
+                language: ruby,
+                category: website)
 Resource.create(name: "Getting Started With Foundation",
                 body: "http://foundation.zurb.com/docs/",
-                language_id: 1,
-                category_id: 3)
+                language: ruby,
+                category: website)
 Resource.create(name: "Search with Sunspot",
                 body: "http://railscasts.com/episodes/278-search-with-sunspot",
-                language_id: 1,
-                category_id: 6)
+                language: ruby,
+                category: video)
 Resource.create(name: "Active Record Basics",
                 body: "http://guides.rubyonrails.org/active_record_basics.html",
-                language_id: 1,
-                category_id: 3)
+                language: ruby,
+                category: website)
 Resource.create(name: "Codecademy",
                 body: "http://www.codecademy.com/dashboard",
-                language_id: 7,
-                category_id: 3)
+                language: other,
+                category: website)
 Resource.create(name: "Stack Overflow",
                 body: "http://stackoverflow.com",
-                language_id: 7,
-                category_id: 3)
+                language: other,
+                category: website)
 Resource.create(name: "Code Fellows",
                 body: "https://www.codefellows.org",
-                language_id: 7,
-                category_id: 2)
+                language: other,
+                category: bootcamp)
 Resource.create(name: "Thinkful",
                 body: "https://www.thinkful.com",
-                language_id: 7,
-                category_id: 3)
+                language: other,
+                category: website)
 Resource.create(name: "How to be a Programmer: A Short, Comprehensive, and Personal Summary",
                 body: "http://samizdat.mines.edu/howto/HowToBeAProgrammer.html#id2792906",
-                language_id: 7,
-                category_id: 4)
+                language: other,
+                category: blog)
 Resource.create(name: "Designer vs Developer: BE THE UNICORN",
                 body: "http://www.slideshare.net/saracannon/wcatl2013",
-                language_id: 7,
-                category_id: 4)
-
-
-
-
-
+                language: other,
+                category: blog)
 
 @user = User.create(email: "user@user.com",
                     password: "password",
